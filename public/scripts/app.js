2eb684214bcb66c502aaf05bdbcd16965623d98c
Priscilla Bakradze <priscillabakradze@hotmail.com>
2022-11-08 15:37:17 -0500

    Created a landing page, navbar and footer

---
+++ public/scripts/app.js
@@ -0,0 +1,3 @@
+// Change the Copyright Year Every Year
+const copyright = `<p> © ${new Date().getFullYear()} Copyright PseudoCodes </p>`;
+document.getElementById("copyright-year").innerHTML = copyright;
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,33 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Welcome!'});
 });
+
+/* GET home page. */
+router.get('/home', function(req, res, next) {
+  res.render('index', { title: 'Welcome!'});
+});
+
+/* GET about page. */
+router.get('/about', function(req, res, next) {
+  res.render('about', { title: 'About'});
+});
+
+/* GET active surveys page. */
+router.get('/active-surveys', function(req, res, next) {
+  res.render('active-surveys', { title: 'Active Surveys'});
+});
+
+/* GET login page. */
+router.get('/login', function(req, res, next) {
+  res.render('login', { title: 'Login'});
+});
+
+/* GET sign up page. */
+router.get('/register', function(req, res, next) {
+  res.render('register', { title: 'Sign Up'});
+});
+
 
 module.exports = router;
