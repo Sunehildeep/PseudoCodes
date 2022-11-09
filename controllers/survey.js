c59adc434e90550114f4bf646880895069b31aff
Sunehildeep Singh <sunehildeepsinghsandhu123@gmail.com>
2022-11-09 17:08:28 -0500

    Added delete and read operations for surveys

--- controllers/survey.js
+++ controllers/survey.js
@@ -13,10 +13,23 @@ module.exports.displayActiveSurveysPage = (req, res, next) => {
         }
         else
         {
-            res.render("survey/active-surveys", { title: "Active Surveys", surveyList: surveyList});
+            res.render("survey/active-surveys", { title: "Active Surveys", surveyList });
         }
     });
 }
+
+module.exports.deleteSurvey = (req, res, next) => {
+
+    let id = req.params.id;
+
+    Survey.deleteOne({ _id: id }, (err) => {
+        if (err) {
+            return console.error(err);
+        } else {
+            res.redirect("/surveys/active-surveys");
+        }
+    });
+};
 
 module.exports.displayCreateSurveyPage = (req, res, next) => {
     res.render("survey/create-survey", { title: "Create a Survey" });
--- routes/survey.js
+++ routes/survey.js
@@ -1,14 +1,14 @@
 let express = require('express');
 let router = express.Router();
 let surveyController = require('../controllers/survey');
-const indexController = require("../controllers");
 
 // GET Route for the Survey List page - READ OPERATION
 router.get("/active-surveys", surveyController.displayActiveSurveysPage);
 
-
 /* GET create survey page. */
 router.get("/create-survey", surveyController.displayCreateSurveyPage);
 
+/* DELETE survey page. */
+router.get("/delete/:id", surveyController.deleteSurvey);
 
 module.exports = router;
