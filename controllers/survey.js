7ba4a12277ec5d89e82f3e95883f0b3894eb3b4c
Rebecca <rebec@DESKTOP-K7N6MO0>
2022-11-10 17:11:13 -0500

    made the create survey page functional

--- controllers/survey.js
+++ controllers/survey.js
@@ -1,36 +1,53 @@
 var express = require("express");
 var router = express.Router();
-let mongoose = require('mongoose');
+let mongoose = require("mongoose");
 
 // connect to our Survey Model
-let Survey = require('../models/survey');
+let Survey = require("../models/survey");
 
 module.exports.displayActiveSurveysPage = (req, res, next) => {
-    Survey.find((err, surveyList) => {
-        if(err)
-        {
-            return console.error(err);
-        }
-        else
-        {
-            res.render("survey/active-surveys", { title: "Active Surveys", surveyList });
-        }
-    });
-}
+  Survey.find((err, surveyList) => {
+    if (err) {
+      return console.error(err);
+    } else {
+      res.render("survey/active-surveys", {
+        title: "Active Surveys",
+        surveyList,
+      });
+    }
+  });
+};
 
 module.exports.deleteSurvey = (req, res, next) => {
+  let id = req.params.id;
 
-    let id = req.params.id;
-
-    Survey.deleteOne({ _id: id }, (err) => {
-        if (err) {
-            return console.error(err);
-        } else {
-            res.redirect("/surveys/active-surveys");
-        }
-    });
+  Survey.deleteOne({ _id: id }, (err) => {
+    if (err) {
+      return console.error(err);
+    } else {
+      res.redirect("/surveys/active-surveys");
+    }
+  });
 };
 
 module.exports.displayCreateSurveyPage = (req, res, next) => {
-    res.render("survey/create-survey", { title: "Create a Survey" });
-}
+  res.render("survey/create-survey", { title: "Create a Survey" });
+};
+
+module.exports.processCreateSurveyPage = (req, res, next) => {
+  let newSurvey = Survey({
+    author: req.body.author,
+    surveyName: req.body.surveyName,
+    startDate: req.body.startDate,
+    closeDate: req.body.closeDate,
+  });
+
+  Survey.create(newSurvey, (err, Survey) => {
+    if (err) {
+      console.log(err);
+      res.end(err);
+    } else {
+      res.redirect("/surveys/active-surveys");
+    }
+  });
+};
--- routes/survey.js
+++ routes/survey.js
@@ -1,12 +1,15 @@
-let express = require('express');
+let express = require("express");
 let router = express.Router();
-let surveyController = require('../controllers/survey');
+let surveyController = require("../controllers/survey");
 
 // GET Route for the Survey List page - READ OPERATION
 router.get("/active-surveys", surveyController.displayActiveSurveysPage);
 
 /* GET create survey page. */
 router.get("/create-survey", surveyController.displayCreateSurveyPage);
+
+/* Post create survey page. */
+router.post("/create-survey", surveyController.processCreateSurveyPage);
 
 /* DELETE survey page. */
 router.get("/delete/:id", surveyController.deleteSurvey);
