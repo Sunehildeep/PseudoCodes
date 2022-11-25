d35c7a757a7258c4952bcb729924776cf3fec980
Sunehildeep Singh <sunehildeepsinghsandhu123@gmail.com>
2022-11-24 22:48:10 -0500

    Fixed auth

--- server/routes/survey.js
+++ server/routes/survey.js
@@ -5,22 +5,14 @@ let passport = require("passport");
 
 let surveyController = require("../controllers/survey");
 
-// function for guard purposes
-function requireAuth(req, res, next) {
-    if(!req.isAuthenticated()) {
-        return res.status(403).send("Unauthorized");
-    }
-    next();
-}
-
 // GET Route for the Survey List page - READ OPERATION
 router.get("/", surveyController.displayActiveSurveysPage);
 
 /* Post create survey page. */
-router.post("/create-survey", requireAuth, surveyController.processCreateSurveyPage);
+router.post("/create-survey", passport.authenticate('jwt', {session: false}), surveyController.processCreateSurveyPage);
 
 /* Post edit survey page. */
-router.post("/edit/:id", requireAuth, surveyController.processEditSurveyPage);
+router.post("/edit/:id", passport.authenticate('jwt', {session: false}), surveyController.processEditSurveyPage);
 
 router.post("/login", surveyController.loginUser);
 
@@ -30,12 +22,12 @@ router.get("/logout", surveyController.logoutUser);
 router.post("/register", surveyController.registerUser);
 
 // update survey
-router.put("/update-survey/:id", requireAuth, surveyController.updateSurvey);
+router.put("/update-survey/:id", passport.authenticate('jwt', {session: false}), surveyController.updateSurvey);
 
 // read survey by id
-router.get("/read-survey/:id", requireAuth, surveyController.displayEditSurveyPage);
+router.get("/read-survey/:id", passport.authenticate('jwt', {session: false}), surveyController.displayEditSurveyPage);
 
 // Delete Survey
-router.delete("/delete-survey/:id", requireAuth, surveyController.performDeleteSurvey);
+router.delete("/delete-survey/:id", passport.authenticate('jwt', {session: false}), surveyController.performDeleteSurvey);
 
 module.exports = router;
