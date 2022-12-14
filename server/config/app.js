90cc30bc4fba82691b5dba80d5a22b8cd61485c9
Sunehildeep Singh <sunehildeepsinghsandhu123@gmail.com>
2022-12-13 19:58:21 -0500

    Combined ejs and angular to run on port 3000

--- server/config/app.js
+++ server/config/app.js
@@ -33,14 +33,22 @@ var surveyRouter = require('../routes/survey.js');
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, '../views'));
-app.set('view engine', 'ejs');
+
+// connect angular to express
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, '../../client/dist/survey')));
+// route all requests to index.html except api requests
+app.get('*', (req, res, next) => {
+  if (!req.originalUrl.startsWith('/api')) {
+    res.sendFile(path.join(__dirname, '../../client/dist/survey/index.html'));
+  } else {
+    next();
+  }
+});
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, '../../public')));
 app.use(express.static(path.join(__dirname, '../../node_modules')));
 app.use(cors());
 // setup express session
@@ -88,20 +96,5 @@ passport.use(strategy);
 
 app.use('/api', surveyRouter);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
 
 module.exports = app;
